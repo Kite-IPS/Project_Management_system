e8886ed8591a48d2dafb63e415396eff8c4c6159
rxnjitth <mranjith2506@gmail.com>
2025-10-12 13:18:23 +0530

    Add trust proxy + final local fixes

--- Server/server.js
+++ Server/server.js
@@ -21,6 +21,7 @@ import paperRouter from './Routes/PaperRoute.js'; // Add this line
 dotenv.config();
 
 const app = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 5000;
 
 // --- Middleware Configuration ---
