28998e8d47e50ab72b66f7bcab4549604a706b0f
rxnjitth <mranjith2506@gmail.com>
2025-10-12 14:19:50 +0530

    `Update server.js to use port 3000 instead of 5000`

--- Server/server.js
+++ Server/server.js
@@ -22,7 +22,7 @@ dotenv.config();
 
 const app = express();
 app.set('trust proxy', 1);
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // --- Middleware Configuration ---
 
